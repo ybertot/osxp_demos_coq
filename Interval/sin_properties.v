815b3d5619f549691ef0b9ecc7e477e776b491f5
Yves Bertot <Yves.Bertot@inria.fr>
2021-10-28 17:59:56 +0200

    adds proofs about real numbers.

---
+++ Interval/sin_properties.v
@@ -0,0 +1,86 @@
+Require Import Reals Lra.
+From Coquelicot Require Import Coquelicot.
+Require Import Interval.Tactic Interval.Plot.
+Open Scope R_scope.
+
+(***************************************************************************)
+(****************************** Real functions *****************************)
+(***************************************************************************)
+
+Check PI.
+
+Lemma PI_approx2 : 3.14 <= PI <= 3.15.
+Proof.
+interval.
+Qed.
+
+Definition psin := ltac:(plot sin 0 PI).
+
+Plot psin.
+
+Definition psinv := ltac:(plot (fun x => sin (1 / x)) (0.01) (0.2)).
+
+Plot psinv.
+(* By comparison, gnuplot has a strange and erroneous behavior close to 0.
+   Here is the command line:
+
+   echo "set xrange [0.01:0.2]; plot sin(1/x)" | gnuplot -p
+
+*)
+
+(* Decimal value of PI is 3.141592653589793238462643383279... *)
+Lemma val_sin_gt0 : 
+  forall x, (0 < sin 3.1415926535) /\
+   (0 < x <= 3.14159265358979 -> 0 < sin x).
+Proof.
+intros x.
+  split.
+    interval.
+set (y := 3.14159265358979).
+assert (x_lt_pi : 0 < y < PI).
+  unfold y; split. 
+    interval.
+  interval with (i_prec 128).
+intros intx.
+apply sin_gt_0.
+  tauto.
+apply Rle_lt_trans with y.
+  tauto.
+tauto.
+Qed.
+
+(* This property can be visualize using gnuplot by typing
+  echo "set xrange [0:10]; plot x, sin(x)" | gnuplot -p
+*)
+Lemma sinx_ltx x : 0 < x -> sin x < x.
+Proof.
+intros xgt0.
+Fail interval.
+apply Rminus_gt_0_lt.
+(* Cut the proof in two parts: after PI/2 and before. *)
+case (Rgt_ge_dec x (PI / 2)).
+  assert (sin x <= 1) by (assert (tmp := SIN_bound x); lra).
+  assert (tmp := PI2_1); lra.
+intros xsmall.
+(* Show the value of the derivative. *)
+assert (der : forall c, 0 <= c <= x -> 
+         derivable_pt_lim (fun x => x - sin x) c
+           (1 - cos c)).
+  intros c _.
+  auto_derive.
+    auto.
+  ring.
+destruct (MVT_cor2 (fun x => x - sin x) (fun x => 1 - cos x) 0 x xgt0 der) as
+  [c [feq cint]].
+rewrite sin_0, !Rminus_0_r in feq; rewrite feq.
+apply Rmult_lt_0_compat; cycle 1.
+  auto.
+enough (cos c < 1) by lra.
+rewrite <- cos_0.
+apply cos_decreasing_1.
+        lra.
+      lra.
+    lra.
+  lra.
+lra.
+Qed.
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# osxp_demos_coq
+# Short demonstrations of Coq
+
+## Equations
+
+  **File**: equations_sort.v
+
+  A small file illustrating how to use the Equations extension to define
+  a simple sorting function and prove its correctness.
+
+  This demonstration was last tested with coq.8.13.2 and
+   coq-equations.1.3~beta2+8.13
+
+  The file `Equations_complements.v` should be compiled first.
+
+## Interval
+
+  **File**: sin_properties.v
+
+  A small file illustrating mathematical proofs about functions, including
+  graphical display of their curves.
+
+  This demonstration was last tested with coq.1.13.2 and coq-interval.4.3.0
+
+## VST
+
+  A live demo of the results in Appel&Bertot2020 about C code implementing
+  a square root function.
+
+  This demonstration was last test with coq.1.13.2 and coq-vst.2.8
+  (compcert.3.9)
